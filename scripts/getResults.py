41f6bed59d3ad0f6ffc1f2686545eadaf1ecb08b
Paul Joannon <hello@pauljoannon.com>
2015-03-06 18:27:31 +0100

    compute france map data from real data

--- scripts/getResults.py
+++ scripts/getResults.py
@@ -17,13 +17,48 @@ def mkdir(path):
     if not os.path.isdir(path):
         os.mkdir(path)
 
-def getFEResults(t):
+def getDptsToUpdate(t):
+    toUpdate = []
+    dpts = []
+
+    r = requests.get(BASE_URL + 'resultatsT{0}/index.xml'.format(t))
+    soup = BeautifulSoup(r.text, "xml")
+
+    for dpt in soup.Election.Departements.findAll("Departement"):
+        mustUpdate = False
+        codDpt3Car = dpt.CodDpt3Car.string
+        lastUpdateDateTime = "{0} {1}".format(dpt.DateDerPubDpt.string, dpt.HeureDerPubDpt.string)
+
+        dpts.append((codDpt3Car, dpt.CodMinDpt.string))
+
+        if dpt.Clos.string == "CLOS":
+            mustUpdate = True
+            jsonFileName = os.path.join(OUT_DIR, "T{0}".format(t), "{0}.json".format(codDpt3Car))
+            if os.path.isfile(jsonFileName):
+                with open(jsonFileName) as f:
+                    try:
+                        if lastUpdateDateTime == json.loads(f.read())['lastUpdateDateTime']:
+                            mustUpdate = False
+                    except ValueError:
+                        pass
+
+        if mustUpdate:
+            toUpdate.append((codDpt3Car, lastUpdateDateTime))
+
+    return (toUpdate, dpts)
+
+def getFEResults(t, fName = None):
+    if fName is None:
+        fName = "FE"
+
+    print("Retrieving results for {0}".format(fName))
+
     results = {
         "inscrits" : None, "abstentions" : None, "nuls" : None,
         "blancs" : None, "votants" : None, "exprimes" : None
     }
 
-    r = requests.get(BASE_URL + 'resultatsT{0}/FE.xml'.format(t))
+    r = requests.get(BASE_URL + 'resultatsT{0}/{1}.xml'.format(t, fName))
     soup = BeautifulSoup(r.text, "xml")
     try:
         tour = soup.Tours.findAll("Tour")[t - 1]
@@ -52,6 +87,29 @@ def getFEResults(t):
         pass
     return results
 
+def getDptResults(t, dptCod3Car, lastUpdateDateTime):
+    return dict(
+        results=getFEResults(t, "{0}/{0}".format(dptCod3Car)),
+        lastUpdateDateTime=lastUpdateDateTime
+    )
+
+def computeFranceMapData(t, dpts):
+    dptsMapData = dict()
+
+    for (dptCod3Car, dptCodMin) in allDpts:
+        jsonFileName = os.path.join(OUT_DIR, 'T{0}'.format(t), "{0}.json".format(dptCod3Car))
+        if (os.path.isfile(jsonFileName)):
+            with open(jsonFileName, "r") as f:
+                try:
+                    dptData = json.loads(f.read())['results']
+                    dptsMapData[dptCodMin] = sorted([(k, v['rapportExprime']) for k, v in dptData.items() if k[:2] == "BC"], key=lambda x: x[1], reverse=True)[0]
+                except ValueError:
+                    pass
+        else:
+            dptsMapData[dptCodMin] = None
+
+    return dptsMapData
+
 if __name__ == "__main__":
     t = 1
     if len(sys.argv) > 1:
@@ -62,5 +120,15 @@ if __name__ == "__main__":
     mkdir(os.path.join(OUT_DIR, 'T1'))
 
     # Retrieve global France results
-    with open(os.path.join(OUT_DIR, 'T' + str(t), 'FE.json'), "w") as f:
-        f.write(json.dumps(getFEResults(t)))
+    # with open(os.path.join(OUT_DIR, 'T{0}'.format(t), 'FE.json'), "w") as f:
+    #     f.write(json.dumps(getFEResults(t)))
+
+    # Retrieve global Dpt results
+    (toUpdate, allDpts) = getDptsToUpdate(t)
+    dptsData = dict()
+    for (dptCod3Car, lastUpdateDateTime) in toUpdate:
+        with open(os.path.join(OUT_DIR, 'T{0}'.format(t), "{0}.json".format(dptCod3Car)), "w") as f:
+            f.write(json.dumps(getDptResults(t, dptCod3Car, lastUpdateDateTime)))
+    # Compute data for France map
+    with open(os.path.join(OUT_DIR, 'T{0}'.format(t), "FEMAP.json"), "w") as f:
+        f.write(json.dumps(computeFranceMapData(t, allDpts)))
