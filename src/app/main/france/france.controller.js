40fc45785e948d2ee3e3e779e62a595b58bf8b65
Paul Joannon <hello@pauljoannon.com>
2015-03-10 11:34:08 +0100

    define map inside a directive

--- src/app/main/france/france.controller.js
+++ src/app/main/france/france.controller.js
@@ -1,50 +1,9 @@
 'use strict';
 
-var FranceCtrl = function($scope, chartData, geojson, mapData) {
+var FranceCtrl = function($scope, leafletData, chartData, geojson, mapData) {
     // Map
-    $scope.map = {
-        center : {
-            lat: 46,
-            lng: 3.5,
-            zoom: 5.2
-        },
-        geojson : {
-            data : geojson,
-            style : {
-                weight: 1,
-                color: "#000",
-                fillColor: "green"
-            },
-            onEachFeature: function(feature, layer) {
-                var color = "#dedede";
-                if (_.has(mapData, feature.properties.code)) {
-                    var data = mapData[feature.properties.code];
-                    if (data != null) {
-                        color = getColorFromNuance(data[0]);
-                        console.debug(feature.properties.nom, data[0]);
-                    } else {
-                        color = "#fff";
-                        console.debug(feature.properties.nom, "No data yet");
-                    }
-                } else {
-                    console.debug(feature.properties.nom, "No data at all");
-                }
-
-                layer.setStyle({
-                    fillColor : color,
-                    fillOpacity : 1
-                });
-            }
-        },
-        defaults : {
-            zoomControl : false,
-            keyboard : false,
-            dragging : false,
-            attributionControl : false,
-            scrollWheelZoom : false,
-            doubleClickZoom : false
-        }
-    };
+    $scope.mapData = mapData;
+    $scope.geojson = geojson;
 
     // Chart
     var lastColumnData = _.omit(chartData, function(v, k) { return k.indexOf("BC") === 0; });
@@ -113,4 +72,4 @@ FranceCtrl.resolve = {
 };
 
 angular.module('departementales2015')
-    .controller('FranceCtrl', ['$scope', 'chartData', 'geojson', 'mapData', FranceCtrl]);
+    .controller('FranceCtrl', ['$scope', 'leafletData', 'chartData', 'geojson', 'mapData', FranceCtrl]);
---
+++ src/components/map/map.directive.js
@@ -0,0 +1,78 @@
+'use strict';
+
+angular.module('departementales2015')
+    .directive('map', [function () {
+        return {
+            restrict : 'EA',
+            scope : {
+                data : '=data',
+                geo : '=geo'
+            },
+            templateUrl : 'components/map/map.html',
+            compile : function() {
+                return {
+                    pre : function($scope) {
+                        $scope.center = {
+                            lat: 46,
+                            lng: 3.5,
+                            zoom: 5.2
+                        };
+
+                        $scope.defaults = {
+                            zoomControl : false,
+                            keyboard : false,
+                            dragging : false,
+                            attributionControl : false,
+                            scrollWheelZoom : false,
+                            doubleClickZoom : false
+                        };
+
+                        $scope.legend = {
+                            position : "bottomleft",
+                            colors : ["#fff", "#dedede"],
+                            labels : ["", ""]
+                        };
+
+                        $scope.events = {
+                            map : {
+                                enable : [ 'click' , 'mousemove' ],
+                                logic : 'emit'
+                            }
+                        };
+
+                        $scope.geojson = {
+                            data : $scope.geo,
+                            style : {
+                                weight: 1,
+                                color: "#000"
+                            },
+                            onEachFeature: function(feature, layer) {
+                                var color = "#dedede";
+                                if (_.has($scope.data, feature.properties.code)) {
+                                    var data = $scope.data[feature.properties.code];
+                                    if (data != null) {
+                                        color = getColorFromNuance(data[0]);
+                                        if (! _.contains($scope.legend.colors, color)) {
+                                            var legend = _.cloneDeep($scope.legend);
+                                            legend.colors.push(color);
+                                            legend.labels.push(getLabelFromNuance(data[0]));
+                                            $scope.legend = legend;
+                                        }
+                                    } else {
+                                        color = "#fff";
+                                    }
+                                }
+
+                                layer.setStyle({
+                                    fillColor : color,
+                                    fillOpacity : 1
+                                });
+                            }
+                        };
+                    },
+
+                    post : function($scope) { }
+                }
+            }
+        }
+    }]);
