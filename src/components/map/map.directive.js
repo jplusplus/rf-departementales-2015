18aeba52d955d0209cfaf1b858926c58de152468
Paul Joannon <hello@pauljoannon.com>
2015-03-16 19:05:28 +0100

    highlight current canton on canton view (map)

--- src/components/map/map.directive.js
+++ src/components/map/map.directive.js
@@ -109,6 +109,13 @@ angular.module('departementales2015')
                                         fillColor : color,
                                         fillOpacity : 1
                                     });
+
+                                    if ($state.is("home.canton") && $stateParams.canton == $scope.getCodeFromData(feature.properties)) {
+                                        layer.setStyle({
+                                            weight : 2,
+                                            opacity: 1
+                                        });
+                                    }
                                 };
                             })()
                         };
