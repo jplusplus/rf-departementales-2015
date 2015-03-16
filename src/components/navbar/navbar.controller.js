a3d0a6980d1c7c41e4e24533fd1467d64b270c8a
Paul Joannon <hello@pauljoannon.com>
2015-03-16 18:51:28 +0100

    fix  var in navbar scope

--- src/components/navbar/navbar.controller.js
+++ src/components/navbar/navbar.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 angular.module('departementales2015')
-  .controller('NavbarCtrl', ['$scope', '$state', function ($scope, $state) {
+  .controller('NavbarCtrl', ['$scope', '$rootScope', '$state', function ($scope, $rootScope, $state) {
+    $scope.t = $rootScope.getT();
+
     $scope.goToT = function(t) {
         if (t == 1 || t == 2) {
+            $scope.t = t;
             $state.go('.', { t : t });
         }
     }
