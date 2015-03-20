63fb1929c6a0b5ae0125096a78cfb7c20794ea89
Paul Joannon <hello@pauljoannon.com>
2015-03-20 11:40:45 +0100

    address

--- src/components/enteryouraddress/enteryouraddress.controller.js
+++ src/components/enteryouraddress/enteryouraddress.controller.js
@@ -32,17 +32,20 @@ angular.module('departementales2015').controller('EnterYourAddressCtrl', ['$scop
                     $http.get(base_url, {
                         params : {
                             format : 'json',
-                            q : data.data[0].lat + "," + data.data[0].lon
+                            q : data.data[0].lat + "," + data.data[0].lon,
+                            addressdetails : 1
                         }
                     }).then(function(data) {
                         if (data.status === 200 && data.data.length > 0) {
-                            var address = data.data[0].display_name;
-                            if (address[0] < '0' || address[0] > '9') {
-                                address = address.split(',').slice(1).join(',');
-                            }
-                            $scope.address = address;
+                            var address = data.data[0].address;
+                            address.house_number = address.house_number || "";
+                            address.town = address.town || address.city || "";
+                            address.footway = address.footway || address.road || address.pedestrian || "";
+                            address.postcode = address.postcode.split(';')[0];
+                            $scope.address = address.house_number + " " + address.footway + ", " + address.postcode + " " + address.town + ", " + address.state;
                         }
                     });
+
                     $state.go('home.canton', {
                         dpt : can.properties.code_dep,
                         canton : can.properties.num_canton,
